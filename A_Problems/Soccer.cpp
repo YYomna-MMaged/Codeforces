e53e9311c312618f4956a197a117bddcea79a3f3
Yomna Maged <14.yomn@gmail.com>
2024-07-06 03:28:11 +0300

    add "Soccer"

---
+++ A_Problems/Soccer.cpp
@@ -0,0 +1,42 @@
+/*
+ * yomn
+ */
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+
+#define int long long
+#define END cout << '\n' ;
+#define fr(n) for(int i = 0 ; i < n ; i++)
+#define s_lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define s_upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define yomn ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+int32_t main() {
+    yomn
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        if ((x1 > y1) == (x2 > y2))
+            cout << "YES";
+        else
+            cout << "NO";
+        END
+    }
+}
