4f8eebaad3f12d817fa1725108e555a34107a4de
Yomna Maged <14.yomn@gmail.com>
2024-07-20 04:17:38 +0300

    X Axis problem

---
+++ A_Problems/X Axis.cpp
@@ -0,0 +1,52 @@
+/*
+ * yomn
+ */
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+
+#define int long long
+#define END cout << '\n' ;
+#define fr(n) for(int i = 0 ; i < n ; i++)
+#define s_lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define s_upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define yomn ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve(){
+    deque< int > xs(3);
+    int ans = 0;
+
+    for (int i = 0; i < 3; ++i) {
+        cin >> xs[i];
+    }
+
+    sort(xs.begin(), xs.end());
+
+    for (int i = 0; i < 3; ++i) {
+        ans += abs(xs[i] - xs[1]);
+    }
+
+    cout << ans;
+    END
+}
+int32_t main() {
+    yomn
+
+    int t = 1;
+    cin >> t;
+
+    while (t--){
+        solve();
+    }
+}
