d0ed6918367615dca959e39b0e0d789404356995
Yomna Maged <14.yomn@gmail.com>
2024-07-22 00:16:55 +0300

    math problem :(

---
+++ A_Problems/ Turtle and Piggy Are Playing a Game.cpp
@@ -0,0 +1,43 @@
+/*
+ * yomn
+ */
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+
+#define int long long
+#define END cout << '\n' ;
+#define fr(n) for(int i = 0 ; i < n ; i++)
+#define s_lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define s_upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define yomn ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve(){
+    int l, r;
+    cin >> l >> r;
+// the number with at least t prime factors is 2^t
+    int ans = log2(r);
+    cout << ans;
+    END
+}
+int32_t main() {
+    yomn
+
+    int t = 1;
+    cin >> t;
+
+    while (t--){
+        solve();
+    }
+}
