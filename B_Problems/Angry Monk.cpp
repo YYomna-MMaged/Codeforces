8bfb71294c02efa14f3a3ff9cbc515d07081805d
Yomna Maged <14.yomn@gmail.com>
2024-07-26 13:10:08 +0300

    هي greedy و math بس 800 عادي

---
+++ B_Problems/Angry Monk.cpp
@@ -0,0 +1,54 @@
+/*
+ * yomn
+ */
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+
+#define int long long
+#define END cout << '\n' ;
+#define fr(n) for(int i = 0 ; i < n ; i++)
+#define s_lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define s_upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define yomn ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve(){
+    int n, k, ans = 0, mx = 0;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; ++i) {
+        int x;
+        cin >> x;
+
+        if (x > 1)
+            ans += (x*2)-1;
+        else
+            ans++;
+        mx = max(mx, x);
+    }
+
+    cout << ans - ((mx*2) - 1);
+    END
+
+}
+int32_t main() {
+    yomn
+
+    int t = 1;
+    cin >> t;
+
+    while (t--){
+        solve();
+    }
+}
